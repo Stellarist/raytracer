58c716d2c23679c64f9a3a1b288c87b468e3c2dc
Stellarist <lzx-117@outlook.com>
2025-04-13 15:38:33 +0800

    clean cmakes

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -1,34 +1,3 @@
-/*
-
- * SPDX-FileCopyrightText: Copyright (c) 2019 - 2024  NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <glad/glad.h>        // Needs to be included before gl_interop
 
 #include <cuda_gl_interop.h>
@@ -649,7 +618,7 @@ void createModule(PathTracerState& state)
 	state.pipeline_compile_options.pipelineLaunchParamsVariableName = "params";
 
 	size_t      inputSize = 0;
-	const char* input = sutil::getInputData(OPTIX_SAMPLE_NAME, OPTIX_SAMPLE_DIR, "optixPathTracer.cu", inputSize);
+	const char* input = sutil::getInputData(OPTIX_SAMPLE_NAME, OPTIX_SAMPLE_DIR, "raytracer.cu", inputSize);
 
 	OPTIX_CHECK_LOG(optixModuleCreate(
 	    state.context,
@@ -901,7 +870,7 @@ int main(int argc, char* argv[])
 		initLaunchParams(state);
 
 		if (outfile.empty()) {
-			GLFWwindow* window = sutil::initUI("optixPathTracer", state.params.width, state.params.height);
+			GLFWwindow* window = sutil::initUI("raytracer", state.params.width, state.params.height);
 			glfwSetMouseButtonCallback(window, mouseButtonCallback);
 			glfwSetCursorPosCallback(window, cursorPosCallback);
 			glfwSetWindowSizeCallback(window, windowSizeCallback);
